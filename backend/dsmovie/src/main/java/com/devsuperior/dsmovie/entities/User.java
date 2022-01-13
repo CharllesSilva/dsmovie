583f35662f4716f4c66868cdf33a9871f8061246
charlles silva <magrohue10@gmail.com>
2022-01-12 22:07:49 -0300

    domain model, database seed

---
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    public User() {
+    }
+
+    public User(Long id, String nome) {
+        this.id = id;
+        this.email = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return email;
+    }
+
+    public void setNome(String nome) {
+        this.email = nome;
+    }
+}
